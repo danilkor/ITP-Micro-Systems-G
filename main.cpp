96cc727ac18351715b60b27b0e5268cdb2769892
= <=>
2025-09-25 15:16:11 +0200

    optimized comments

--- main.cpp
+++ main.cpp
@@ -1,54 +1,3 @@
-// async_subscribe.cpp
-//
-// This is a Paho MQTT C++ client, sample application.
-//
-// This application is an MQTT publisher/subscriber using the C++
-// asynchronous client interface, demonstrating how you can share a client
-// between multiple threads.
-//
-// The app will count the number of "data" messages arriving at the broker
-// and then emit "events" with updated counts. A data message is any on a
-// "data/#" topic, and counts are emitted on the "events/count" topic. It
-// emits an event count around once every ten data messages.
-//
-// Note that this is a fairly contrived example, and it could be done much
-// more easily in a single thread. It is meant to demonstrate how you can
-// share a client amongst threads if and when that's a proper thing to do.
-//
-// At this time, there is a single callback or consumer queue for all
-// incoming messages, so you would typically only have one thead receiving
-// messages, although it _could_ send messages to multiple threads for
-// processing, perhaps based on the topics. It could be common, however, to
-// want to have multiple threads for publishing.
-//
-// This example demonstrates:
-//  - Creating a client and sharing it across threads using a shared_ptr<>
-//  - Using one thread to receive incoming messages from the broker and
-//    another thread to publish messages to it.
-//  - Connecting to an MQTT se./asrver/broker.
-//  - Automatic reconnect
-//  - Publishing messages
-//  - Subscribing to multiple topics
-//  - Using the asynchronous message consumer
-//  - Signaling consumer from another thread
-//
-
-/*******************************************************************************
- * Copyright (c) 2020-2025 Frank Pagliughi <fpagliughi@mindspring.com>
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * and Eclipse Distribution License v1.0 which accompany this distribution.
- *
- * The Eclipse Public License is available at
- *    http://www.eclipse.org/legal/epl-v20.html
- * and the Eclipse Distribution License is available at
- *   http://www.eclipse.org/org/documents/edl-v10.php.
- *
- * Contributors:
- *    Frank Pagliughi - initial implementation and documentation
- *******************************************************************************/
-
 #include <cctype>
 #include <chrono>
 #include <cstdlib>
@@ -63,6 +12,7 @@
 using namespace std;
 using namespace std::chrono;
 
+/////////////////////////////////////////////////////////////////////////////
 const std::string DFLT_SERVER_ADDRESS{"tcp://eu1.cloud.thethings.network:1883"};
 const std::string CLIENT_ID{"itp-team-1b0123314213124123"};
 const string mqqt_username = "itp-project-1@ttn";
@@ -114,7 +64,6 @@ int main(int argc, char* argv[])
 
 
         // Start another thread to read incoming topics
-
         std::thread{[client] {
             while (true) {
             cout << "Waiting for a message\n";
