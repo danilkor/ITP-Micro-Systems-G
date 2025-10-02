77b9f794c5c7be6404dac9b4e3076e9f0e1712f9
Lanthan <danilkor4444@gmail.com>
2025-10-02 15:02:00 +0200

    added json

--- main.cpp
+++ main.cpp
@@ -8,28 +8,31 @@
 #include <thread>
 
 #include "mqtt/async_client.h"
+#include <nlohmann/json.hpp>
 
-#include "ftxui/component/captured_mouse.hpp"  // for ftxui
-#include "ftxui/component/component.hpp"  // for Button, Horizontal, Renderer
-#include "ftxui/component/component_base.hpp"      // for ComponentBase
-#include "ftxui/component/screen_interactive.hpp"  // for ScreenInteractive
-#include "ftxui/dom/elements.hpp"  // for separator, gauge, text, Element, operator|, vbox, border
-
-
+#include "ftxui/component/captured_mouse.hpp"     // for ftxui
+#include "ftxui/component/component.hpp"          // for Button, Horizontal, Renderer
+#include "ftxui/component/component_base.hpp"     // for ComponentBase
+#include "ftxui/component/screen_interactive.hpp" // for ScreenInteractive
+#include "ftxui/dom/elements.hpp"                 // for separator, gauge, text, Element, operator|, vbox, border
 
 using namespace std;
 using namespace std::chrono;
 using namespace ftxui;
 
-/////////////////////////////////////////////////////////////////////////////
+// for convenience
+using json = nlohmann::json;
+
+/////////////////////////////  ////////////////////////////////////////////////
 const std::string DFLT_SERVER_ADDRESS{"tcp://eu1.cloud.thethings.network:1883"};
 const std::string CLIENT_ID{"itp-team-1b0123314213124123"};
 const string mqqt_username = "itp-project-1@ttn";
 const string device_name = "uno-0004a30b001c1b03";
 
 /////////////////////////////////////////////////////////////////////////////
-
-atomic_int value = 50;
+const string downlink_topic = "v3/" + mqqt_username + "/devices/" + device_name + "/down/push";
+const string uplink_topic = "v3/" + mqqt_username + "/devices/" + device_name + "/up";
+atomic_int last_temp = 0;
 atomic_bool led_status = false;
 
 // This is a helper function to create a button with a custom style.
@@ -38,40 +41,53 @@ atomic_bool led_status = false;
 // We are using `center` to center the text inside the button, then `border` to
 // add a border around the button, and finally `flex` to make the button fill
 // the available space.
-ButtonOption Style() {
-  auto option = ButtonOption::Animated();
-  option.transform = [](const EntryState& s) {
-    auto element = text(s.label);
-    if (s.focused) {
-      element |= bold;
-    }
-    return element | center | borderEmpty | flex;
-  };
-  return option;
+ButtonOption Style()
+{
+    auto option = ButtonOption::Animated();
+    option.transform = [](const EntryState &s)
+    {
+        auto element = text(s.label);
+        if (s.focused)
+        {
+            element |= bold;
+        }
+        return element | center | borderEmpty | flex;
+    };
+    return option;
 }
 
-void publish(mqtt::async_client_ptr client, const string topic, const string payload) {
+void publish(mqtt::async_client_ptr client, const string topic, const string payload)
+{
+    cout << payload << "\n";
     auto msg = mqtt::make_message(topic, payload);
     msg->set_qos(1);
     client->publish(msg);
     led_status = (payload == "1");
 }
 
-void set_led_status(mqtt::async_client_ptr client, bool status) {
-    string topic = "v3/" + mqqt_username + "/devices/" + device_name + "/down/replace";
-    string payload = status? "{\"downlinks\":[{\"f_port\": 15,\"frm_payload\":\"vu8=\",\"priority\": \"NORMAL\"}]}" : "0";
-    publish(client, topic, payload);
+void set_led_status(mqtt::async_client_ptr client, bool status)
+{
+    json json_payload;
+    json_payload["downlinks"][0] = {{"f_port", 15},
+                                  {"frm_payload", {{"app", "building"}, {"type", "ledcontrol"}, {"led", (int)status}}},
+                                  {"priority", "NORMAL"}};
+    json_payload["downlinks"][0]["frm_payload"] = json_payload["downlinks"][0]["frm_payload"].dump();
+    cout << json_payload["downlinks"][0]["frm_payload"];
+    publish(client, downlink_topic, json_payload.dump());
 }
 
-const char* api_key;
-int main(int argc, char* argv[])
+const char *api_key;
+int main(int argc, char *argv[])
 {
     // Get the API key
     api_key = getenv("TTN_API_KEY");
-    if (!api_key) {
+    if (!api_key)
+    {
         cerr << "Error: Please set the environment variable TTN_API_KEY";
         exit(1);
-    } else {
+    }
+    else
+    {
         cout << "Using API key: " << string(api_key).substr(0, 8) << "..." << endl;
     }
 
@@ -88,10 +104,11 @@ int main(int argc, char* argv[])
                         .automatic_reconnect(seconds(2), seconds(30))
                         .finalize();
 
-    auto TOPICS = mqtt::string_collection::create({"#","data"});
-    const vector<int> QOS{0, 1};
+    auto TOPICS = mqtt::string_collection::create({uplink_topic});
+    const vector<int> QOS{0};
 
-    try {
+    try
+    {
         // Start consuming _before_ connecting, because we could get a flood
         // of stored messages as soon as the connection completes since
         // we're using a persistent (non-clean) session with the broker.
@@ -99,57 +116,61 @@ int main(int argc, char* argv[])
 
         cout << "Connecting to the MQTT server at " << address << "..." << flush;
         auto rsp = client->connect(connOpts)->get_connect_response();
-        cout << "OK\n" << endl;
+        cout << "OK\n"
+             << endl;
 
         // Subscribe if this is a new session with the server
         if (!rsp.is_session_present())
             client->subscribe(TOPICS, QOS);
 
-
         // Start another thread to read incoming topics
-        std::thread{[client] {
-            while (true) {
-            auto msg = client->consume_message();
-            if (!msg) {
-                // Exit if the consumer was shut down
-                if (client->consumer_closed())
-                    break;
-                continue;
-            }
-        }
-        }}.detach();
-        
+        std::thread{[client]
+                    {
+                        while (true)
+                        {
+                            auto msg = client->consume_message();
+                            if (!msg)
+                            {
+                                // Exit if the consumer was shut down
+                                if (client->consumer_closed())
+                                    break;
+                                continue;
+                            }
+                            auto parsed = json::parse(msg->to_string());
+                            cout << parsed << "\n";
+                        }
+                    }}
+            .detach();
 
-        value = 50;
+        // Set default values
+        last_temp = 50;
         led_status = false;
 
         // clang-format off
         auto btn_led_on = Button("ON", [&] { std::thread(set_led_status, client, true).detach(); }, Style());
         auto btn_led_off = Button("OFF", [&] { std::thread(set_led_status, client, false).detach(); }, Style());
         // clang-format on
-      
+
         // The tree of components. This defines how to navigate using the keyboard.
         // The selected `row` is shared to get a grid layout.
         int row = 0;
         auto buttons = Container::Vertical({
             Container::Horizontal({btn_led_on, btn_led_off}, &row) | flex,
         });
-      
+
         // Modify the way to render them on screen:
-        auto component = Renderer(buttons, [&] {
-          return vbox({
-                    text("LED = " + std::to_string(led_status)),
-                    separator(),
-                    buttons->Render() | flex,
-                }) |
-                flex | border;
-        });
-      
+        auto component = Renderer(buttons, [&]
+                                  { return vbox({
+                                               text("LED = " + std::to_string(led_status)),
+                                               separator(),
+                                               buttons->Render() | flex,
+                                           }) |
+                                           flex | border; });
+
         auto screen = ScreenInteractive::FitComponent();
         screen.Loop(component);
 
-
-
+        
         client->stop_consuming();
         // Close the counter and wait for the publisher thread to complete
         cout << "\nShutting down..." << flush;
@@ -159,7 +180,8 @@ int main(int argc, char* argv[])
         client->disconnect();
         cout << "OK" << endl;
     }
-    catch (const mqtt::exception& exc) {
+    catch (const mqtt::exception &exc)
+    {
         cerr << exc.what() << endl;
         return 1;
     }
